9437766d40d656d0dfebe3bab821ba821c0c4ab2
JannatunNayma2007 <74146600+JannatunNayma2007@users.noreply.github.com>
2022-01-25 20:57:20 +0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,73 +1,51 @@
-const Engine = Matter.Engine;
-const Render = Matter.Render;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
-const Body = Matter.Body;
-const Composites = Matter.Composites;
-const Composite = Matter.Composite;
+var canvas;
+var backgroundImage, car1_img, car2_img, track;
+var fuelImage, powerCoinImage, lifeImage;
+var obstacle1Image, obstacle2Image;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers, car1, car2, fuels, powerCoins, obstacles;
+var blastImage;
+var cars = [];
 
-let engine;
-let world;
-var ground, bridge;
-var leftWall, rightWall;
-var jointPoint;
-var jointLink;
-
-var stones = [];
+function preload() {
+  backgroundImage = loadImage("./assets/background.png");
+  car1_img = loadImage("./assets/car1.png");
+  car2_img = loadImage("./assets/car2.png");
+  track = loadImage("./assets/track.jpg");
+  fuelImage = loadImage("./assets/fuel.png");
+  powerCoinImage = loadImage("./assets/goldCoin.png");
+  obstacle1Image = loadImage("./assets/obstacle1.png");
+  obstacle2Image = loadImage("./assets/obstacle2.png");
+  lifeImage = loadImage("./assets/life.png");
+  blastImage = loadImage("./assets/blast.png");
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  engine = Engine.create();
-  world = engine.world;
-  frameRate(80);
-
-  ground = new Base(0, height - 10, width * 2, 20, "#795548", true);
-  leftWall = new Base(300, height / 2 + 50, 600, 100, "#8d6e63", true);
-  rightWall = new Base(width - 300, height / 2 + 50, 600, 100, "#8d6e63", true);
-
-  /*bridge = new Base(15, { x: width / 2 - 400, y: height / 2 });
-  jointPoint = new Base(width - 600, height / 2 + 10, 40, 20, "#8d6e63", true);*/
-
-  bridge = new Bridge(15, { x: width / 2 - 400, y: height / 2 });
-  jointPoint = new Base(width - 600, height / 2 + 10, 40, 20, "#8d6e63", true);
-
-  /*bridge = new Base(15, { x: width / 2 - 400, y: height / 2 });
-  jointPoint = new Bridge(width - 600, height / 2 + 10, 40, 20, "#8d6e63", true);*/
-
-  /*bridge = new Bridge(15, { x: width / 2 - 400, y: height / 2 });
-  jointPoint = new Bridge(width - 600, height / 2 + 10, 40, 20, "#8d6e63", true);*/
-
-  
-  Matter.Composite.add(bridge.body, jointPoint);
-
-  //Matter.Composite.add(jointPoint);
-  
-  //Matter.Composite.add(jointPoint, bridge.body);
-  
-  //Matter.Composite.add(bridge.body);
-
-
-  jointLink = new Link(bridge, jointPoint);
-
-  for (var i = 0; i <= 8; i++) {
-    var x = random(width / 2 - 200, width / 2 + 300);
-    var y = random(-10, 140);
-    var stone = new Stone(x, y, 80, 80);
-    stones.push(stone);
-  }
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
 }
 
 function draw() {
-  background(51);
-  Engine.update(engine);
-
-  ground.show();
-  bridge.show();
-  leftWall.show();
-  rightWall.show();
-
-  for (var stone of stones) {
-    stone.show();
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
   }
+
+  if (gameState === 1) {
+    game.play();
+  }
+
+  if (gameState === 2) {
+    game.showLeaderboard();
+    game.end();
+    
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
